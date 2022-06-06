0be400ff00e561962f167c1166d90b6fe41e6bc8
Moritz Roessler <moritz.roessler@cosuno.de>
2022-06-06 13:52:07 +0100

    feat: rename math functions

--- src/CalculationResult.ts
+++ src/CalculationResult.ts
@@ -1,6 +1,12 @@
-﻿export class CalculationResult {
-    public isValid: boolean = false;
-    public errorPosition: number | null = null;
-    public errorMessage: string | null = null;
-    public result: number = NaN;
+﻿type Tree = Record<string, Tree>;
+
+export type CalculationOptions = {
+  includeAst: boolean;
+};
+export class CalculationResult {
+  public ast: Tree = null;
+  public isValid: boolean = false;
+  public errorPosition: number | null = null;
+  public errorMessage: string | null = null;
+  public result: number = NaN;
 }
