16ff84e48414272f202d8a722424fcc913049549
Moritz Roessler <moritz.roessler@cosuno.de>
2022-06-07 22:03:29 +0100

    Revert "fix: move build to postinstall"
    This reverts commit 4e1030d6f1e0f44a76041e87ad462483761b754a.

---
+++ dist/CalculationResult.js
@@ -0,0 +1,10 @@
+export class CalculationResult {
+    constructor() {
+        this.ast = null;
+        this.isValid = false;
+        this.errorPosition = null;
+        this.errorMessage = null;
+        this.result = NaN;
+    }
+}
+//# sourceMappingURL=CalculationResult.js.map
